225b45ef37f6ad58a01a469e7d6a285bef5082d0
Alex Zherdev <42341013+CNDspace@users.noreply.github.com>
2020-05-25 18:57:40 +0300

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -8,6 +8,7 @@ sudo dnf install epel-release -y
 sudo dnf update epel-release -y
 sudo dnf config-manager --set-enabled PowerTools
 sudo dnf install https://forensics.cert.org/cert-forensics-tools-release-el8.rpm -y
+sudo dnf update -y
 sudo dnf install ghidra -y
 sudo dnf install radare2 -y
 sudo dnf install cutter-re -y
